8c563d7c1164946bd532016aff7c4a39783369c4
ghostrecom <53305213+ghostrecom@users.noreply.github.com>
2022-04-28 16:58:38 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version "adamant"
+games {"rdr3"}
+lua54 'yes'
+
+author 'Halcon'
+description 'Temporizador RedM'
+version 'Version 1.0'
+
+client_script {
+
+    "client.lua",
+}
+
+exports{
+    "run",
+}
+
+
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
